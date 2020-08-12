eaa7f21e9d78ed80560cba14475297e94d333f8b
Pete <pop.peter89@gmail.com>
2020-08-12 16:20:30 +0200

    Success

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*function calcAmount() {
+    let amountInput = document.querySelector("input[name='amount-input']")
+    let price = 100;
+    let amount = parseInt(amountInput.value) * price;
+    let showAmount = document.querySelector("span.show-amount");
+
+    showAmount.innerHTML = amount;
+}
+*/
